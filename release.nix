464461a62e940906cab661beb905f637a81d5ad8
Claes Wallin (韋嘉誠) <claes.wallin@greatsinodevelopment.com>
2018-11-23 16:28:36 +0800

    Problem: release: Not building what we can on darwin
    We can't build a Nix system on darwin[0], but we should build the
    components of one.
    
    Solution: Add explicit jobs for the various things.
    
    [0] Yet! At some point we should make nix-darwin modules

--- release.nix
+++ release.nix
@@ -24,6 +24,16 @@ genJobs null pinnedNixos // {
   oldstable = genJobs <nixos-oldstable> <nixos-oldstable/nixos>;
   stable = genJobs <nixos-stable> <nixos-stable/nixos>;
   nixos-unstable = genJobs <nixos-unstable> <nixos-unstable/nixos>;
+  x86_64-darwin = let
+    darwin.pkgs = import (import ./pins/nixpkgs) { system = "x86_64-darwin"; };
+    tezos-baking-platform = import (import ./pins/tezos-baking-platform) {
+      nixpkgs = darwin.pkgs;
+    };
+    inherit (import <nixpkgs> {}) lib;
+  in (lib.listToAttrs (map (name: lib.nameValuePair name tezos-baking-platform.tezos.${name}.kit)
+                           (attrNames tezos-baking-platform.tezos))) // {
+    inherit (import ./pkgs { inherit (darwin) pkgs; }) backerei;
+  };
 } // (import <nixpkgs> {}).lib.optionalAttrs isTravis {
   travisOrder = [ "system" ];
 }
